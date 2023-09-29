ba5ebdd44d0b1b3482f13437222683c6c79db22f
AlejandroRico <alejandro.ricog05@gmail.com>
2023-09-29 10:39:49 +0200

    .

--- src/Sesion2.java
+++ src/Sesion2.java
@@ -3,17 +3,19 @@ public class Sesion2 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int numero;
-        do {
+        System.out.println("Escribe un número entero numero mayor que cinco y menor que 30");
+        numero = teclado.nextInt();
+        while((numero < 5) || (numero > 30)){
             System.out.println("Escribe un número entero numero mayor que cinco y menor que 30");
             numero = teclado.nextInt();
+        }
             for (int i = 1; i <= numero; i++) {
-                for(int j = 1; j <= i; j++) {
-                    System.out.print("*");
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
                 }
                 System.out.println();
 
-            }
-        } while (numero > 5 && numero < 30);
+        }
 
     }
 }
