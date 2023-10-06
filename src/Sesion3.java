3ea5a5d5eba5b374d8bdb76351f39cbc1e0f9010
AlejandroRico <alejandro.ricog05@gmail.com>
2023-10-06 12:28:32 +0200

    He quitado el menu

--- src/Sesion3.java
+++ src/Sesion3.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Sesion3 {
     static Scanner teclado = new Scanner(System.in);
 
@@ -28,39 +29,43 @@ public class Sesion3 {
                 case 5:
                     System.out.println("Programa finalizado");
             }
-        }while (opcion!=5);
+        } while (opcion != 5);
     }
-    public static int menu(){
-            System.out.println("1. Dibuja un triangulo rectangulo.");
-            System.out.println("2. Dibuja un cuadrado.");
-            System.out.println("3. Multiplicar dos numeros.");
-            System.out.println("4. Escribir una tabla de multiplicar.");
-            System.out.println("5. Finalizar ejecucion.");
-            int opcion = leerNumero(1,5);
+
+    public static int menu() {
+        System.out.println("1. Dibuja un triangulo rectangulo.");
+        System.out.println("2. Dibuja un cuadrado.");
+        System.out.println("3. Multiplicar dos numeros.");
+        System.out.println("4. Escribir una tabla de multiplicar.");
+        System.out.println("5. Finalizar ejecucion.");
+        int opcion = leerNumero(1, 5);
         return opcion;
     }
-    public static int leerNumero (int minimo, int maximo){
+
+    public static int leerNumero(int minimo, int maximo) {
         int numero = 0;
         do {
-            System.out.print("Escribe un numero comprendido entre " + minimo + " y " + maximo+": ");
+            System.out.print("Escribe un numero comprendido entre " + minimo + " y " + maximo + ": ");
             numero = teclado.nextInt();
-            if((numero<minimo)||(numero>maximo)){
+            if ((numero < minimo) || (numero > maximo)) {
                 System.out.println("Numero incorrecto");
             }
-        }while ((numero<minimo)||(numero>maximo));
-        System.out.println("El numero introducido es "+numero);
+        } while ((numero < minimo) || (numero > maximo));
+        System.out.println("El numero introducido es " + numero);
         return numero;
     }
-    public static char leerCaracter(){
+
+    public static char leerCaracter() {
         System.out.print("Introduce un caracter no alfabetico ni numerico: ");
-        char caracter= teclado.next().charAt(0);
-        while(Character.isAlphabetic(caracter)||Character.isDigit(caracter)){
-                System.out.println("Numero incorrecto, introduce un caracter que no sea ni numero ni letra");
-                caracter = teclado.next().charAt(0);
+        char caracter = teclado.next().charAt(0);
+        while (Character.isAlphabetic(caracter) || Character.isDigit(caracter)) {
+            System.out.println("Numero incorrecto, introduce un caracter que no sea ni numero ni letra");
+            caracter = teclado.next().charAt(0);
         }
         return caracter;
-        }
-    public static void dibujarTriangulo (int base, char caracter){
+    }
+
+    public static void dibujarTriangulo(int base, char caracter) {
         for (int i = 1; i <= base; i++)
         //Este for sirve para escribir saltar las lineas que indica el numero
         {
@@ -71,7 +76,8 @@ public class Sesion3 {
             System.out.println();
         }
     }
-    public static void dibujarCuadrado(int lado,char caracter){
+
+    public static void dibujarCuadrado(int lado, char caracter) {
         for (int i = 1; i <= lado; i++) {
             //Saltar de fila
             for (int j = 1; j <= lado; j++) {
@@ -81,16 +87,18 @@ public class Sesion3 {
             System.out.println();
         }
     }
-    public static int multiplicarIterativo(int numero1, int numero2){
+
+    public static int multiplicarIterativo(int numero1, int numero2) {
         int resultado = 0;
         for (int i = 0; i < numero1; i++) {
             resultado += numero2;
             //La multiplicacion es sumar n veces un numero, por lo que repetimos i veces (numero1) la suma de numero2
         }
-        System.out.println("La multipliacion de "+numero1+" * "+numero2+" = "+resultado);
+        System.out.println("La multipliacion de " + numero1 + " * " + numero2 + " = " + resultado);
         return resultado;
     }
-    public static void tablaDeMultiplicar(int numero){
+
+    public static void tablaDeMultiplicar(int numero) {
         for (int i = 0; i <= 10; i++) {
             int multiplicacion = numero * i;
             System.out.printf("%3d X %3d = %3d\n", numero, i, multiplicacion);
